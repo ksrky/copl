6c0daab4f3f0e0ef33ce3ead5372b3c6a9e0960b
ksrky <xgrk9475@gmail.com>
2025-07-19 13:35:40 +0900

    wip

--- src/EvalNatExp.hs
+++ src/EvalNatExp.hs
@@ -39,51 +39,115 @@ deriveLess :: Int -> Int -> Derivation
 deriveLess i1 i2 = Derivation (Lt i1 i2 (i1 < i2)) "B-Lt" []
 
 deriveEvalTo :: Exp -> Maybe Derivation
-deriveEvalTo (I i) = pure $ Derivation (EvalTo (I i) (VInt i)) "E-Int" []
-deriveEvalTo (B b) = pure $ Derivation (EvalTo (B b) (VBool b)) "E-Bool" []
+deriveEvalTo (I i) = pure $ Derivation (EvalTo (I i) (RV (VInt i))) "E-Int" []
+deriveEvalTo (B b) = pure $ Derivation (EvalTo (B b) (RV (VBool b))) "E-Bool" []
 deriveEvalTo (Add e1 e2)
     | Just p1 <- deriveEvalTo e1
-    , (EvalTo _ (VInt i1)) <- conclusion p1
+    , (EvalTo _ (RV (VInt i1))) <- conclusion p1
     , Just p2 <- deriveEvalTo e2
-    , (EvalTo _ (VInt i2)) <- conclusion p2
+    , (EvalTo _ (RV (VInt i2))) <- conclusion p2
     , let p3 = derivePlus i1 i2
     , (Plus _ _ i3) <- conclusion p3
-    = pure $ Derivation (EvalTo (Add e1 e2) (VInt i3)) "E-Plus" [p1, p2, p3]
+    = pure $ Derivation (EvalTo (Add e1 e2) (RV (VInt i3))) "E-Plus" [p1, p2, p3]
+    | Just p1 <- deriveEvalTo e1
+    , (EvalTo _ (RV VBool{})) <- conclusion p1
+    = Just $ Derivation (EvalTo (Add e1 e2) RError) "E-PlusBoolL" [p1]
+    | Just p2 <- deriveEvalTo e2
+    , (EvalTo _ (RV VBool{})) <- conclusion p2
+    = Just $ Derivation (EvalTo (Add e1 e2) RError) "E-PlusBoolR" [p2]
+    | Just p1 <- deriveEvalTo e1
+    , (EvalTo _ RError) <- conclusion p1
+    = Just $ Derivation (EvalTo (Add e1 e2) RError) "E-PlusErrorL" [p1]
+    | Just p2 <- deriveEvalTo e2
+    , (EvalTo _ RError) <- conclusion p2
+    = Just $ Derivation (EvalTo (Add e1 e2) RError) "E-PlusErrorR" [p2]
 deriveEvalTo (Sub e1 e2)
     | Just p1 <- deriveEvalTo e1
-    , (EvalTo _ (VInt i1)) <- conclusion p1
+    , (EvalTo _ (RV (VInt i1))) <- conclusion p1
     , Just p2 <- deriveEvalTo e2
-    , (EvalTo _ (VInt i2)) <- conclusion p2
+    , (EvalTo _ (RV (VInt i2))) <- conclusion p2
     , let p3 = deriveMinus i1 i2
     , (Minus _ _ i3) <- conclusion p3
-    = pure $ Derivation (EvalTo (Sub e1 e2) (VInt i3)) "E-Minus" [p1, p2, p3]
+    = pure $ Derivation (EvalTo (Sub e1 e2) (RV (VInt i3))) "E-Minus" [p1, p2, p3]
+    | Just p1 <- deriveEvalTo e1
+    , (EvalTo _ (RV VBool{})) <- conclusion p1
+    = Just $ Derivation (EvalTo (Sub e1 e2) RError) "E-MinusBoolL" [p1]
+    | Just p2 <- deriveEvalTo e2
+    , (EvalTo _ (RV VBool{})) <- conclusion p2
+    = Just $ Derivation (EvalTo (Sub e1 e2) RError) "E-MinusBoolR" [p2]
+    | Just p1 <- deriveEvalTo e1
+    , (EvalTo _ RError) <- conclusion p1
+    = Just $ Derivation (EvalTo (Sub e1 e2) RError) "E-MinusErrorL" [p1]
+    | Just p2 <- deriveEvalTo e2
+    , (EvalTo _ RError) <- conclusion p2
+    = Just $ Derivation (EvalTo (Sub e1 e2) RError) "E-MinusErrorR" [p2]
 deriveEvalTo (Mul e1 e2)
     | Just p1 <- deriveEvalTo e1
-    , (EvalTo _ (VInt i1)) <- conclusion p1
+    , (EvalTo _ (RV (VInt i1))) <- conclusion p1
     , Just p2 <- deriveEvalTo e2
-    , (EvalTo _ (VInt i2)) <- conclusion p2
+    , (EvalTo _ (RV (VInt i2))) <- conclusion p2
     , let p3 = deriveTimes i1 i2
     , (Times _ _ i3) <- conclusion p3
-    = pure $ Derivation (EvalTo (Mul e1 e2) (VInt i3)) "E-Times" [p1, p2, p3]
+    = pure $ Derivation (EvalTo (Mul e1 e2) (RV (VInt i3))) "E-Times" [p1, p2, p3]
+    | Just p1 <- deriveEvalTo e1
+    , (EvalTo _ (RV VBool{})) <- conclusion p1
+    = Just $ Derivation (EvalTo (Mul e1 e2) RError) "E-TimesBoolL" [p1]
+    | Just p2 <- deriveEvalTo e2
+    , (EvalTo _ (RV VBool{})) <- conclusion p2
+    = Just $ Derivation (EvalTo (Mul e1 e2) RError) "E-TimesBoolR" [p2]
+    | Just p1 <- deriveEvalTo e1
+    , (EvalTo _ RError) <- conclusion p1
+    = Just $ Derivation (EvalTo (Mul e1 e2) RError) "E-TimesErrorL" [p1]
+    | Just p2 <- deriveEvalTo e2
+    , (EvalTo _ RError) <- conclusion p2
+    = Just $ Derivation (EvalTo (Mul e1 e2) RError) "E-TimesErrorR" [p2]
 deriveEvalTo (Less e1 e2)
     | Just p1 <- deriveEvalTo e1
-    , (EvalTo _ (VInt i1)) <- conclusion p1
+    , (EvalTo _ (RV (VInt i1))) <- conclusion p1
     , Just p2 <- deriveEvalTo e2
-    , (EvalTo _ (VInt i2)) <- conclusion p2
+    , (EvalTo _ (RV (VInt i2))) <- conclusion p2
     , let p3 = deriveLess i1 i2
     , (Lt _ _ b) <- conclusion p3
-    = pure $ Derivation (EvalTo (Less e1 e2) (VBool b)) "E-Less" [p1, p2, p3]
+    = pure $ Derivation (EvalTo (Less e1 e2) (RV (VBool b))) "E-Lt" [p1, p2, p3]
+    | Just p1 <- deriveEvalTo e1
+    , (EvalTo _ (RV VBool{})) <- conclusion p1
+    = Just $ Derivation (EvalTo (Less e1 e2) RError) "E-LtBoolL" [p1]
+    | Just p2 <- deriveEvalTo e2
+    , (EvalTo _ (RV VBool{})) <- conclusion p2
+    = Just $ Derivation (EvalTo (Less e1 e2) RError) "E-LtBoolR" [p2]
+    | Just p1 <- deriveEvalTo e1
+    , (EvalTo _ RError) <- conclusion p1
+    = Just $ Derivation (EvalTo (Less e1 e2) RError) "E-LtErrorL" [p1]
+    | Just p2 <- deriveEvalTo e2
+    , (EvalTo _ RError) <- conclusion p2
+    = Just $ Derivation (EvalTo (Less e1 e2) RError) "E-LtErrorR" [p2]
 deriveEvalTo (Ite e1 e2 e3)
     | Just p1 <- deriveEvalTo e1
-    , (EvalTo _ (VBool True)) <- conclusion p1
+    , (EvalTo _ (RV (VBool True))) <- conclusion p1
     , Just p2 <- deriveEvalTo e2
-    , (EvalTo _ v2) <- conclusion p2
-    = Just $ Derivation (EvalTo (Ite e1 e2 e3) v2) "E-IfT" [p1, p2]
+    , (EvalTo _ (RV v2)) <- conclusion p2
+    = Just $ Derivation (EvalTo (Ite e1 e2 e3) (RV v2)) "E-IfT" [p1, p2]
     | Just p1 <- deriveEvalTo e1
-    , (EvalTo _ (VBool False)) <- conclusion p1
+    , (EvalTo _ (RV (VBool False))) <- conclusion p1
     , Just p3 <- deriveEvalTo e3
-    , (EvalTo _ v3) <- conclusion p3
-    = Just $ Derivation (EvalTo (Ite e1 e2 e3) v3) "E-IfF" [p1, p3]
+    , (EvalTo _ (RV v3)) <- conclusion p3
+    = Just $ Derivation (EvalTo (Ite e1 e2 e3) (RV v3)) "E-IfF" [p1, p3]
+    | Just p1 <- deriveEvalTo e1
+    , (EvalTo _ (RV VInt{})) <- conclusion p1
+    = Just $ Derivation (EvalTo (Ite e1 e2 e3) RError) "E-IfInt" [p1]
+    | Just p1 <- deriveEvalTo e1
+    , (EvalTo _ RError) <- conclusion p1
+    = Just $ Derivation (EvalTo (Ite e1 e2 e3) RError) "E-IfError" [p1]
+    | Just p1 <- deriveEvalTo e1
+    , (EvalTo _ (RV (VBool True))) <- conclusion p1
+    , Just p2 <- deriveEvalTo e2
+    , (EvalTo _ RError) <- conclusion p2
+    = Just $ Derivation (EvalTo (Ite e1 e2 e3) RError) "E-IfTError" [p1, p2]
+    | Just p1 <- deriveEvalTo e1
+    , (EvalTo _ (RV (VBool False))) <- conclusion p1
+    , Just p3 <- deriveEvalTo e3
+    , (EvalTo _ RError) <- conclusion p3
+    = Just $ Derivation (EvalTo (Ite e1 e2 e3) RError) "E-IfEError" [p1, p3]
 deriveEvalTo _ = Nothing
 
 main :: IO ()
--- src/Syntax.hs
+++ src/Syntax.hs
@@ -3,13 +3,13 @@ module Syntax where
 data Val = VInt Int | VBool Bool
     deriving (Eq)
 
-data Res = RVal Val | RError
+data Res = RV Val | RError
     deriving (Eq)
 
 data Exp = I Int | B Bool | Add Exp Exp | Sub Exp Exp | Mul Exp Exp | Less Exp Exp | Ite Exp Exp Exp
     deriving (Eq)
 
-data Judgement = EvalTo Exp Val
+data Judgement = EvalTo Exp Res
                | Plus Int Int Int
                | Minus Int Int Int
                | Times Int Int Int
@@ -24,8 +24,8 @@ instance Show Val where
     show (VBool b) = showBool b
 
 instance Show Res where
-    show (RVal v) = show v
-    show RError   = "error"
+    show (RV v) = show v
+    show RError = "error"
 
 instance Show Exp where
     show (I n)       = show n
@@ -37,7 +37,7 @@ instance Show Exp where
     show (Ite e1 e2 e3) = "if " ++ show e1 ++ " then " ++ show e2 ++ " else " ++ show e3
 
 instance Show Judgement where
-    show (EvalTo e n)     = show e ++ " evalto " ++ show n
+    show (EvalTo e r)     = show e ++ " evalto " ++ show r
     show (Plus n1 n2 n3)  = show n1 ++ " plus " ++ show n2 ++ " is " ++ show n3
     show (Minus n1 n2 n3) = show n1 ++ " minus " ++ show n2 ++ " is " ++ show n3
     show (Times n1 n2 n3) = show n1 ++ " times " ++ show n2 ++ " is " ++ show n3
